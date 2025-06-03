ef9f5a161bc18416c39b21b5f1fb80513a6bfadc
aswinnt <aswinmalamakkavu@gmail.com>
2025-06-03 17:35:45 +0530

    added static root

--- teamsync/teamsync/settings.py
+++ teamsync/teamsync/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from decouple import config
 from datetime import timedelta
 from pathlib import Path
@@ -271,6 +272,7 @@ cloudinary.config(
 )
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
