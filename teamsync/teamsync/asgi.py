ca2579aff515f273b68dbc41923b097774f3da31
aswinnt <aswinmalamakkavu@gmail.com>
2025-06-04 11:36:02 +0530

    updated asgi

--- teamsync/teamsync/asgi.py
+++ teamsync/teamsync/asgi.py
@@ -1,20 +1,19 @@
 import os
 import django
 
-# Ensure the Django settings module is set and apps registry is loaded
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamsync.settings')
 django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from realtime.middleware import JWTAuthMiddleware
-import realtime.routing
+from realtime.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": JWTAuthMiddleware(
         URLRouter(
-            realtime.routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
